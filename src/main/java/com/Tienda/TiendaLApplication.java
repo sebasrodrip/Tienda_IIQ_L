0ae1743647555272e61609efa21e605e1ca87145
sebastian <sebastian@sebastian-virtualbox>
2022-05-26 11:52:41 -0600

    Creacion del proyecto

---
+++ src/main/java/com/Tienda/TiendaLApplication.java
@@ -0,0 +1,13 @@
+package com.Tienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaLApplication.class, args);
+	}
+
+}
