0ccde0b257b02be9a159f958d1397450b4366d22
sebastian <sebastian@sebastian-virtualbox>
2022-08-15 17:47:17 -0600

    Finalizacion clase semana 11

--- src/main/java/com/Tienda/controller/ClienteController.java
+++ src/main/java/com/Tienda/controller/ClienteController.java
@@ -12,39 +12,47 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Controller
 @Slf4j
 public class ClienteController {
-    
+
     @Autowired
     private ClienteService clienteService;
 
     @GetMapping("/cliente/listado")
-    public String inicio(Model model) {       
+    public String inicio(Model model) {
         var clientes = clienteService.getClientes();
+
+        var limiteTotal = 0;
+        for (var c : clientes) {
+            limiteTotal += c.getCredito().getLimite();
+        }
+        model.addAttribute("limiteTotal", limiteTotal);
+        model.addAttribute("totalClientes", clientes.size());
+
         model.addAttribute("clientes", clientes);
         return "/cliente/listado";
     }
-    
+
     @GetMapping("/cliente/nuevo")
-    public String nuevoCliente(Cliente cliente){
+    public String nuevoCliente(Cliente cliente) {
         return "/cliente/modificar";
     }
-    
+
     @PostMapping("/cliente/guardar")
-    public String guardarCliente(Cliente cliente){
+    public String guardarCliente(Cliente cliente) {
         clienteService.save(cliente);
         return "redirect:/cliente/listado";
     }
-    
+
     @GetMapping("/cliente/modificar/{idCliente}")
-    public String modificarCliente(Cliente cliente, Model model){
+    public String modificarCliente(Cliente cliente, Model model) {
         cliente = clienteService.getCliente(cliente);
-        model.addAttribute("cliente",cliente);
+        model.addAttribute("cliente", cliente);
         return "/cliente/modificar";
     }
-    
+
     @GetMapping("/cliente/eliminar/{idCliente}")
-    public String eliminarCliente(Cliente cliente){
+    public String eliminarCliente(Cliente cliente) {
         clienteService.delete(cliente);
         return "redirect:/cliente/listado";
     }
-    
+
 }
